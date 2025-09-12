ce30f5108ee85d41e07659de7aafad79eabc1bcb
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-12 04:27:16 +0000

    Advanced JavaScript

---
+++ 1_Scope in Js/script.js
@@ -0,0 +1,29 @@
+/*
+Scope topics = Functional scope, global scope and Block scope
+
+1. Function Scope = Function ke andar hi youse ho sakti hai
+
+2. Global Scope = Poore code mein kahi bhi youse ho sakti hai
+
+3. Block Scope = {} Curly braces main hii youse ho sakti hai
+ */
+
+
+
+/* Example of Function Scope :
+*/
+function abcd () {
+    var a  = 12;
+}
+
+
+/* Example of Global Scope :
+*/
+var b = 22
+
+
+/* Example of Block Scope :
+*/
+{
+    var c = 33;
+}
