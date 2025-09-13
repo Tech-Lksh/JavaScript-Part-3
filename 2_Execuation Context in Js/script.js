a17f86ecb217f6f5d1876f7ef2f166c8f5add77a
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-13 03:13:05 +0000

    js

---
+++ 2_Execuation Context in Js/script.js
@@ -0,0 +1,7 @@
+/*
+    Execution Content Topic - 
+*/
+
+/*
+    JavaScript sabse phle jaise hii apka function dethta hai sabse phle js banaata hai execuatiion context, ye ek process hai jo ki do different phases mein chalta hai, memory phanse and doosre ka naam hai execution phanse
+*/
